99b970e9a4765d1e877830ce64d809bfd5087090
Kajal Rajak <rajakkajal219@gmail.com>
2026-01-18 12:28:00 +0530

    Add Login part

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,10 @@ import { user_logged_in } from "./storage.js";
 if (!user_logged_in) {
     window.location.href = 'login.html'
 }
+//log out concept when logout iit goes to login window
+const logOutBtn=document.getElementById("logOutBtn");
+logOutBtn.addEventListener("click", ()=>{
+      // Remove the "logIn" item from localStorage
+  // This clears the user's login state, effectively logging them out
+  localStorage.removeItem("logIn")
+})
